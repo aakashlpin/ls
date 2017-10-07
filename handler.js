d57b6e6d12d7178e4bec966135a3b173e6388b90
Aakash Goel <aakash.lpin@gmail.com>
2017-10-07 11:54:05 +0530

    more logging for easy CloudWatch debugging

--- handler.js
+++ handler.js
@@ -1,17 +1,22 @@
 'use strict';
 const request = require('request');
 const scrape = require('./src/scrapers');
+const qs = require('querystring');
 
 module.exports.add = (event, context, callback) => {
-  const { url } = JSON.parse(event.body);
+  console.log(event.body);
+  const { url } = qs.parse(event.body);
+  console.log(qs.parse(event.body));
   scrape(url)
     .then(function (data) {
+      console.log(data);
       callback(null, {
         statusCode: 200,
         body: JSON.stringify(data),
       });
     })
     .catch(function (e) {
+      console.log('something bad happened', e);
       callback(null, e);
     });
 }
@@ -28,6 +33,7 @@ module.exports.run = (event, context, callback) => {
 
 module.exports.crawl = (event, context, callback) => {
   const productInfo = JSON.parse(event.body);
+  console.log(productInfo);
   const { url, id, seller } = productInfo;
   scrape(url, seller)
     .then(function (scrapedInfo) {
@@ -41,11 +47,22 @@ module.exports.crawl = (event, context, callback) => {
         headers: {
           "Content-type": "application/json",
         },
-      }, (requestError, response, body) => {
-        callback(requestError, body);
       });
+      callback(null, scrapedInfo);
     })
     .catch(function (e) {
+      console.log('issues in crawl', e);
+      request({
+        url: `${process.env.ZEIT_SERVER}/api/prices/${id}`,
+        method: 'POST',
+        json: {
+          productInfo: productInfo,
+        },
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+
       callback(null, e);
     })
 }
